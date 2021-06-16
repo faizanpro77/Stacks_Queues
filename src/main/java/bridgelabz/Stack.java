cd78acef751fbbc270d0d888c5590205651d1aaf
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-16 12:45:38 +0530

    REMOVE: remove lines of code

--- src/main/java/bridgelabz/Stack.java
+++ src/main/java/bridgelabz/Stack.java
@@ -1,14 +1,2 @@
 package bridgelabz;
 
-public class Stack<T> {
-    MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
-
-    public void push(INode newNode) {
-        myLinkedList.add(newNode);
-    }
-
-    public INode peek() {
-        return myLinkedList.head;
-    }
-
-}
