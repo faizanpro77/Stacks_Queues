ac7bfe76704bc5d6dcad07e3acdf71443f054a75
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-16 12:49:42 +0530

    MODIFIED: add functionality to create stack

--- src/main/java/bridgelabz/Stack.java
+++ src/main/java/bridgelabz/Stack.java
@@ -1,2 +1,13 @@
 package bridgelabz;
 
+public class Stack<T> {
+    MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+
+    public void push(INode newNode) {
+        myLinkedList.add(newNode);
+    }
+
+    public INode peek() {
+        return myLinkedList.head;
+    }
+}
